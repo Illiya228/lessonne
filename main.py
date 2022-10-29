19b40b77d302283a7473c23368280e62556f4203
Студент <student@ITStep.local>
2022-10-29 16:38:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+class Human:
+    def __init__(self, name="ruhfc", job = None, home = None, car = None ):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car = car
+        self.money = 1000
+        self.gladness = 50
+        self.satiety = satiety
+        self.hunger = 50
+    def get_home(self):
+        pass
+
+    def get_car(self):
+        pass
+
+    def get_job(self):
+        pass
+
+    def get_food(self):
+        pass
+
+    def shoping(self):
+        pass
+
+    def chill(self):
+        pass
+
+    def clearn_home(self):
+        pass
+
+    def day_indexes(self,day):
+        pass
+
+    def is_alive(self):
+        pass
+
+    def live(self,day):
+        pass
+
+class Auto:
+    def __init__(self, brand_list):
+        self.brand = random.choice(list(brand.list))
+        self.strength = brand_list[self.brand]['strength']
+        self.fuel = brand_list[self.brand]['fuel']
+        self.comsumption = brand_list[self.brand]['comsumption']
+        self.passengers = []
+    def add_passengers (self, human):
+        self.passengers
